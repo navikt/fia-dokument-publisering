a6f0554fed0d90e7c70de713d6ee8e6a47c0343b
Thomas Dufourd <thomas.dufourd@gmail.com>
2025-06-10 16:36:40 +0200

    Bruk com.gradleup.shadow plugin for å bygge applikasjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.20"
     kotlin("plugin.serialization") version "2.1.20"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 group = "no.nav"
