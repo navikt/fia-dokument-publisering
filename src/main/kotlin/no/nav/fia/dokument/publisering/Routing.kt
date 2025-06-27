f3812def9e7ed705ba9908096613f9267709b48f
nimajimale <nima0512@gmail.com>
2025-06-27 15:10:02 +0200

    Fjern duplisert helsesjekk endepunkter
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>
    Co-authored-by: Sindre Larsen <sindre.larsen@nav.no>

--- src/main/kotlin/no/nav/fia/dokument/publisering/Routing.kt
+++ src/main/kotlin/no/nav/fia/dokument/publisering/Routing.kt
@@ -2,24 +2,11 @@ package no.nav.fia.dokument.publisering
 
 import ApplikasjonsHelse
 import io.ktor.server.application.Application
-import io.ktor.server.application.log
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import no.nav.fia.dokument.publisering.helse.helse
 
 fun Application.configureRouting(applikasjonsHelse: ApplikasjonsHelse) {
     routing {
-        get("internal/isalive") {
-            call.application.log.info("Hei loggen!")
-            call.respondText("Hello World!")
-        }
-
-        get("internal/isready") {
-            call.application.log.info("Hei loggen!")
-            call.respondText("Hello World!")
-        }
-
         helse(lever = { applikasjonsHelse.alive }, klar = { applikasjonsHelse.ready })
     }
 }
