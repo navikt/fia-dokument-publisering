79c706c917d28ec85142dfe13fd9969e58570fbd
Christian Klem <christian.klem@nav.no>
2026-03-06 08:39:56 +0100

    Fiks for sårbarheter i jackson-core  og litt opprydding i constraints

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,32 +77,16 @@ dependencies {
 
     constraints {
         implementation("org.mozilla:rhino") {
-            version {
-                require("1.8.1")
-            }
-            because(
-                "versjonser < 1.8.1 har sårbarhet. inkludert i verapdf 1.28.2",
-            )
+            version { require("1.8.1") }
+            because("versjonser < 1.8.1 har sårbarhet. inkludert i verapdf 1.28.2")
         }
-        implementation("io.netty:netty-codec-http2") {
-            version {
-                require("4.2.9.Final")
-            }
-            because(
-                "ktor-server-netty har sårbar versjon",
-            )
+        implementation("com.fasterxml.jackson.core:jackson-core") {
+            version { require("2.21.1") }
+            because("versjoner < 2.21.1 har sårbarhet. inkludert i ktor-server-auth:3.4.0")
         }
-        testImplementation("org.apache.commons:commons-compress") {
-            version {
-                require("1.28.0")
-            }
-            because("testcontainers har sårbar versjon")
-        }
-        testImplementation("commons-io:commons-io") {
-            version {
-                require("2.21.0")
-            }
-            because("testcontainers har sårbar versjon")
+        implementation("tools.jackson.core:jackson-core") {
+            version { require("3.1.0") }
+            because("versjoner < 3.1.0 har sårbarhet. inkludert i logstash-logback-encoder:9.0")
         }
     }
 }
